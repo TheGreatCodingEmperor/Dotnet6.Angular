e6478106c8bb2a825598551853c570eb41e59161
TheGreatCodingEmperor <thehandsomeboyno.1@gmail.com>
2022-02-26 16:13:14 +0800

    primeng shared module

--- Portal/ClientApp/src/app/app.module.ts
+++ Portal/ClientApp/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './modules/shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    SharedModule,
+    NoopAnimationsModule,
     BrowserModule,
     AppRoutingModule
   ],
